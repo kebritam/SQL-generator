651df17b36d6f308da929f6959f2c5975b876729
Amirali Tavakoli <amiralitavakoli80@gmail.com>
2021-04-06 19:40:26 +0430

    adding limit and offset to query-- wrap StringBuilder

--- src/main/java/SQL.java
+++ src/main/java/SQL.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author kebritam
@@ -9,7 +11,11 @@ import java.util.*;
 public class SQL {
 
     private static final Builder builder = new Builder();
+    private static final String AND = ") \nAND (";
+    private static final String OR = ") \nOR (";
+
     private SQL() {
+        throw new AssertionError("constructor should not be called");
     }
 
     public static Builder builder() {
@@ -41,13 +47,23 @@ public class SQL {
             return this;
         }
 
+        public Builder limit(int limit) {
+            statement.limit = limit;
+            return this;
+        }
+
+        public Builder offset(int offset) {
+            statement.offset = offset;
+            return this;
+        }
+
         public Builder and() {
-            statement.andor.add(") \nAND (");
+            statement.andor.add(AND);
             return this;
         }
 
         public Builder or() {
-            statement.andor.add(") \nOR (");
+            statement.andor.add(OR);
             return this;
         }
 
@@ -55,7 +71,6 @@ public class SQL {
             statement.orderBys.addAll(Arrays.asList(orderCondition));
             return this;
         }
-
 
         public Builder update(String table) {
             statement.queryType = Statement.QueryType.UPDATE;
@@ -97,8 +112,48 @@ public class SQL {
             SELECT, INSERT, DELETE, UPDATE
         }
 
+        private QueryType queryType;
+        private boolean distinct;
+        private int limit = -1;
+        private int offset = -1;
+        private final List<String> settings = new LinkedList<>();
+        private final List<String> selects = new LinkedList<>();
+        private final List<String> tables = new LinkedList<>();
+        private final List<String> whereConditions = new LinkedList<>();
+        private final List<String> orderBys = new LinkedList<>();
+        private final List<String> columns = new LinkedList<>();
+        private final List<String> values = new LinkedList<>();
+        private List<String> andor = new LinkedList<>();
+
+        private static class StringWrapper {
+            private final StringBuilder builder;
+            private boolean isEmpty;
+
+            public StringWrapper() {
+                this.builder = new StringBuilder();
+            }
+
+            public StringWrapper append(String string) {
+                if (isEmpty && string.length() > 0) {
+                    isEmpty = false;
+                }
+                builder.append(string);
+                return this;
+            }
+
+            public StringWrapper append(int number) {
+                builder.append(number);
+                return this;
+            }
+
+            @Override
+            public String toString() {
+                return builder.toString();
+            }
+        }
+
         private String query() {
-            StringBuilder builder = new StringBuilder();
+            StringWrapper builder = new StringWrapper();
             String query;
             switch (queryType) {
                 case SELECT:
@@ -116,13 +171,15 @@ public class SQL {
                 default:
                     return null;
             }
-            cleanLists();
+            cleanStatement();
             return query;
         }
 
-        private void cleanLists() {
+        private void cleanStatement() {
             distinct = false;
             queryType = null;
+            offset = -1;
+            limit = -1;
             settings.clear();
             selects.clear();
             tables.clear();
@@ -133,16 +190,17 @@ public class SQL {
             andor.clear();
         }
 
-        private String selectQuery(StringBuilder builder) {
+        private String selectQuery(StringWrapper builder) {
             sqlAppender(builder, "SELECT" + (distinct ? " DISTINCT" : ""), selects, "", "", ", ");
             sqlAppender(builder, "FROM", tables, "", "", ", ");
             sqlAppender(builder, "WHERE", whereConditions, "(", ")", " AND ");
             sqlAppender(builder, "ORDER BY", orderBys, "", "", ", ");
+            limitOffset(builder, limit, offset);
 
             return builder.toString();
         }
 
-        private String insertQuery(StringBuilder builder) {
+        private String insertQuery(StringWrapper builder) {
             sqlAppender(builder, "INSERT INTO", tables, "", "", "");
             sqlAppender(builder, "", columns, "(", ")", ", ");
             sqlAppender(builder, "VALUES", values, "(", ")", ", ");
@@ -150,54 +208,55 @@ public class SQL {
             return builder.toString();
         }
 
-        private String deleteQuery(StringBuilder builder) {
+        private String deleteQuery(StringWrapper builder) {
             sqlAppender(builder, "DELETE FROM", tables, "", "", "");
             sqlAppender(builder, "WHERE", whereConditions, "(", ")", " AND ");
+            limitOffset(builder, limit, offset);
 
             return builder.toString();
         }
 
-        private String updateQuery(StringBuilder builder) {
+        private String updateQuery(StringWrapper builder) {
             sqlAppender(builder, "UPDATE", tables, "", "", "");
             sqlAppender(builder, "SET", settings, "", "", ", ");
             sqlAppender(builder, "WHERE", whereConditions, "(", ")", " AND ");
+            limitOffset(builder, limit, offset);
 
             return builder.toString();
         }
 
-        public static void sqlAppender(StringBuilder builder, String keyword, List<String> parts, String open,
-                                       String close, String separator) {
-            final String AND = ") \nAND (";
-            final String OR = ") \nOR (";
-
-            if (!parts.isEmpty()) {
-                builder.append("\n");
+        private void limitOffset(StringWrapper builder, int limit, int offset) {
+            if (limit >= 0) {
+                builder.append(" LIMIT ")
+                        .append(limit);
             }
-            builder.append(keyword)
-                    .append(" ")
-                    .append(open);
-            String last = "";
-            for (int i = 0, count = parts.size(); i < count ; i++) {
-                String part = parts.get(i);
-                if (i > 0 && !part.equals(AND) && !part.equals(OR) && !last.equals(AND) && !last.equals(OR)) {
-                    builder.append(separator);
-                }
-                builder.append(part);
-                last = part;
+            if (offset >= 0) {
+                builder.append(" OFFSET ")
+                        .append(offset);
             }
-            builder.append(close);
         }
 
-        private QueryType queryType;
-        private boolean distinct;
-        private final List<String> settings = new LinkedList<>();
-        private final List<String> selects = new LinkedList<>();
-        private final List<String> tables = new LinkedList<>();
-        private final List<String> whereConditions = new LinkedList<>();
-        private final List<String> orderBys = new LinkedList<>();
-        private final List<String> columns = new LinkedList<>();
-        private final List<String> values = new LinkedList<>();
-        private List<String> andor = new LinkedList<>();
+        public void sqlAppender(StringWrapper builder, String keyword, List<String> parts, String open,
+                                       String close, String separator) {
+            if (!parts.isEmpty()) {
 
+                if (!builder.isEmpty) {
+                    builder.append("\n");
+                }
+                builder.append(keyword)
+                        .append(" ")
+                        .append(open);
+                String last = "";
+                for (int i = 0, count = parts.size(); i < count; i++) {
+                    String part = parts.get(i);
+                    if (i > 0 && !part.equals(AND) && !part.equals(OR) && !last.equals(AND) && !last.equals(OR)) {
+                        builder.append(separator);
+                    }
+                    builder.append(part);
+                    last = part;
+                }
+                builder.append(close);
+            }
+        }
     }
 }
